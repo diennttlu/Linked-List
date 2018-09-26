fb7d22ef2697b18da89432ebffaa21d5818b23fa
Tu Dien <tudien1609@gmail.com>
2018-09-26 10:30:58 -0400

    update file main

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main()
 {
- 	List<int> l;
- 	l.PushBack(1);
- 	l.PushBack(2);
- 	l.PushBack(3);
- 	l.PushBack(4);
- 	l.PushBack(5);
- 	l.PushBack(6);
+ 	List<int> l1;
+ 	l1.PushBack(3);
 
- 	l.Change(2,3);
- 	l.PrintAll();
+ 	l1.PushBack(5);
+
+ 	l1.PushBack(7);
+
+ 	List<int> l2 = l1;
+
+ 	l1.DeleteAtPos(2);
+
+	l1.PrintAll();
+	l2.PrintAll();
+
 	return 0;
 }
